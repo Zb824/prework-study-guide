7df04b37de7dc0ef238399c22bba6cfde86b5253
Zb824 <zborders28@outlook.com>
2022-12-09 10:59:16 -0600

    Merge branch 'feature/add-js' of https://github.com/Zb824/prework-study-guide

--- prework-study-guide/assets/assets/assets/script.js
+++ prework-study-guide/assets/assets/assets/script.js
@@ -1,10 +1,10 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 for (var x = 0; x < topics.length; x++) {
- console.log(topics[x]);
-}
 
-/*
+    console.log(topics[x]);
+   }
+   
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -16,4 +16,4 @@ if (topic === 'HTML') {
 } else {
  console.log('Please try again!');
 }
-*/
+
