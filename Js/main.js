b5ceba9a8a2aecadde9cccb6c560e6e9ce73ae52
Zb824 <zborders28@outlook.com>
2022-12-20 14:52:12 -0600

    portfolio

---
+++ Js/main.js
@@ -0,0 +1,36 @@
+console.log("hello world");
+
+var favoriteFood = "tacos";
+ 
+function countTheLetters(str){
+  console.log("Here is your count: ", str.length)
+
+}
+
+countTheLetters(favoriteFood); 
+
+
+console.log(favoriteFood);
+
+favoriteFood = "pizza";
+console.log("letter count", favoriteFood.length)
+
+
+
+
+var age = 29; //number
+
+console.log(age)
+
+var isZachAwesome = true; //camel-casing
+
+var students = ["bob", "bill", "jim" ]
+
+console.log (students. length );
+
+
+
+// data types
+// string : anything is quotes
+//number; any numerical value, never in quotes
+// boolean; true or false
---
+++ inclass/02-Stu_Script_ConsoleLog/Unsolved/script.js
@@ -0,0 +1,10 @@
+// Open up the console to see this log
+console.log("Your external JavaScript file is linked 🎉");
+
+
+var favoriteFood = "tacos"; 
+favoriteFood = "steak";
+
+console.log(favoriteFood);
+
+var numberOfStudents = 30; //number
